a33a33aa0753e464619cce318e40bd93a77f1a9b
bolatashim <bolat.ashim@gmail.com>
2017-05-12 21:49:05 +0900

    jquery added

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -1,4 +1,14 @@
 
-var canvas = $("#amazingCanvas");
+//var canvas = ;
+//var canvas = document.getElementById("amazingCanvas");
+var ctx = $("#amazingCanvas")[0].getContext("2d");
 
-var ctx = canvas.getContext("2d");
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(50, 50, 50, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+drawBall();
